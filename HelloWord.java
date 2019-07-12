d0fa2034f54f2793be43f214d5b08b7f5197189e
LapterGrsd <48401984+JeffyLapter@users.noreply.github.com>
2019-07-12 16:35:56 +0800

    the Source to test the environment.

---
+++ HelloWord.java
@@ -0,0 +1,10 @@
+/*
+This is a testing file which used to test the JAVA environment.
+*/
+public class HelloWorld
+{
+    public static void main(String []args)
+    {
+        System.out.println("Hello World");
+    }
+}
