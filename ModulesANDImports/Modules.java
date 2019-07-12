31caa27db39af7b52ce2629b9e3a329edaf06ee8
LapterGrsd <48401984+JeffyLapter@users.noreply.github.com>
2019-07-12 17:06:34 +0800

    Create Modules.java

---
+++ ModulesANDImports/Modules.java
@@ -0,0 +1,13 @@
+/*
+##This folder illistrated a brief example that how Different modules work together.
+*/
+package MoudulesANDImports; //THE Definition of the package name;
+public class Modules
+{
+    int Data_in_Modules=0;
+    String name="TEST_DATA";
+    public void Print_in_Modules()
+    {
+        System.out.println(name+":"+Data_in_Modules);
+    }
+}
