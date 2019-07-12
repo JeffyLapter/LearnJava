9211a7b0dee26684fe8c63c41a1cc5c5047d1fd2
LapterGrsd <48401984+JeffyLapter@users.noreply.github.com>
2019-07-12 17:05:37 +0800

    Delete Main.java

--- Modules&Imports/Main.java
+++
@@ -1,9 +0,0 @@
-package ModulesANDImports;
-public class Main
-{
-    public static void main(String []args)
-    {
-        Data_in_Modules objt=new Data_in_Modules();
-        objt.Print_in_Modules()
-    }
-}
