d6a8a14abc7f91f49b421649e9af97305147992e
LapterGrsd <48401984+JeffyLapter@users.noreply.github.com>
2019-07-12 17:07:00 +0800

    Create Main.java

---
+++ ModulesANDImports/Main.java
@@ -0,0 +1,9 @@
+package ModulesANDImports;
+public class Main
+{
+    public static void main(String []args)
+    {
+        Data_in_Modules objt=new Data_in_Modules();
+        objt.Print_in_Modules()
+    }
+}
